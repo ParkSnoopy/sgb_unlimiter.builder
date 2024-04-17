ea14e52a8a1c0fa13651d63a2ee2947cafa2eba3
mil-acc-github <147524262+mil-acc-github@users.noreply.github.com>
2024-04-17 20:22:50 +0900

    add icon build script&asset

--- Rust/build.rs
+++ Rust/build.rs
@@ -1,6 +1,20 @@
-use std::io;
+use {
+    std::{
+        env,
+        io,
+    },
+    winres::WindowsResource,
+};
 
 fn main() -> io::Result<()> {
     static_vcruntime::metabuild();
+
+    if env::var_os("CARGO_CFG_WINDOWS").is_some() {
+        WindowsResource::new()
+            // This path can be absolute, or relative to your crate root.
+            .set_icon("assets/icon.ico")
+            .compile()?;
+    }
+    
     Ok(())
 }
