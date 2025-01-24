a2a0984ad1459fa5283b13b60d55e496c91b9921
ParkSnoopy <117149837+ParkSnoopy@users.noreply.github.com>
2025-01-24 12:30:04 +0900

    update: UAC privilege from `highestAvailable` to `requireAdministrator`

--- windows-msvc/build.rs
+++ windows-msvc/build.rs
@@ -2,8 +2,6 @@ use eyre::Result;
 
 use std::env;
 
-
-
 fn main() -> Result<()> {
     if let Some(_) = env::var_os("CARGO_FEATURE_NOSCRIPT") {
         return Ok(());
@@ -13,17 +11,19 @@ fn main() -> Result<()> {
 
     let mut res = winres::WindowsResource::new();
 
-    res.set_manifest(r#"
+    res.set_manifest(
+        r#"
 <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
   <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
     <security>
       <requestedPrivileges>
-        <requestedExecutionLevel level="highestAvailable" uiAccess="false" />
+        <requestedExecutionLevel level="requireAdministrator" uiAccess="false" />
       </requestedPrivileges>
     </security>
   </trustInfo>
 </assembly>
-    "#);
+    "#,
+    );
     res.set_icon("assets/icon.ico");
 
     res.compile()?;
